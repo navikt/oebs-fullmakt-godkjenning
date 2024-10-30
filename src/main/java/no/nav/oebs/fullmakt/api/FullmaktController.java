c47a076b77c77cde114e024fa4cab627b360d9db
rasmusvknav <rasmus.vedholm.krog@nav.no>
2024-10-30 13:26:11 +0100

    Token endring for fullmakt endepunkt fra parameter til header

--- src/main/java/no/nav/oebs/fullmakt/api/FullmaktController.java
+++ src/main/java/no/nav/oebs/fullmakt/api/FullmaktController.java
@@ -3,13 +3,15 @@ package no.nav.oebs.fullmakt.api;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.oebs.fullmakt.Application;
 import no.nav.oebs.fullmakt.config.FullmaktSwagger;
 import no.nav.oebs.fullmakt.config.SwaggerConfig;
 import no.nav.oebs.fullmakt.service.FullmaktService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 
 @Slf4j
 @RestController
@@ -17,25 +19,28 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
 @Tag(name = SwaggerConfig.FULLMAKT, description = "Fullmakt API")
 public class FullmaktController {
-    private FullmaktService fullmaktservice;
-    public FullmaktController(FullmaktService fullmaktservice) { //,
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    private final FullmaktService fullmaktservice;
+    public FullmaktController(FullmaktService fullmaktservice) {
         this.fullmaktservice = fullmaktservice;
     }
 
     @GetMapping(path = "/fullmakt")
     @FullmaktSwagger
     public String hentFullmakter(
-            @RequestParam(name="org_id", defaultValue = "202") Integer org_id,
-            @RequestParam(name="segmentverdi", required = false)
-            @Parameter(description = "f.eks. 159101", required = false) String segmentverdi,
-            @RequestParam(name="segment", required = false)
-            @Parameter(description = "f.eks. Kostnadssted", required = false) String segment,
+            @RequestParam(name = "org_id", defaultValue = "202") Integer org_id,
+            @RequestParam(name = "segmentverdi", required = false)
+            @Parameter(description = "f.eks. 159101") String segmentverdi,
+            @RequestParam(name = "segment", required = false)
+            @Parameter(description = "f.eks. Kostnadssted") String segment,
             @RequestParam(name = "minBelopsgrense", required = false)
-            @Parameter(description = "f.eks. 25000", required = false) Integer minBelopsgrense,
-            @RequestParam(name="token", required = true)
-            @Parameter(description = "tokenet fra Azure AD", required = true) String token) {
+            @Parameter(description = "f.eks. 25000") Integer minBelopsgrense,
+            @RequestHeader(name = "Authorization") String authorizationHeader) {
 
-            // Fulmakter hentes fra Fullmaktregister
-            return fullmaktservice.hentFullmaktService(segmentverdi, segment, minBelopsgrense, token);
+        logger.info("Authorization header: " + authorizationHeader);
+        // Fulmakter hentes fra Fullmaktregister
+        return fullmaktservice.hentFullmaktService(segmentverdi, segment, minBelopsgrense, authorizationHeader);
     }
 }
+
